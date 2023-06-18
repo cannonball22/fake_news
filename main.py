5cd8fb1dacc05687d4cc1af05c360560cbdbabe7
Adham mostafa <43731337+cannonball22@users.noreply.github.com>
2023-06-18 16:50:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from helper import fake_news_det
 
 
 query_params = st.experimental_get_query_params()
-print(query_params)
+#print(query_params)
 
 #st.write(query_params)
-st.title('Fake news dashboard')
+#st.title('Fake news dashboard')
 #print(query_params["input"][0])
 
 #fake_news_det(query_params["input"][0])
 try:
     response = fake_news_det(query_params["input"][0])
-    st.write(response)
+    #st.write(response)
     st.json({'response': response})
 except:
     st.write("error")
